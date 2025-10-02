e19ea2c9ce658faeb69796ca9c048af1b08282b6
Snow <bis23-smsumba@mubas.ac.mw>
2025-10-02 13:28:36 +0200

    added some files

---
+++ zdata.sql
@@ -0,0 +1,208 @@
+INSERT INTO Constituency (ConstituencyName, DistrictID) VALUES
+ ('Lilongwe City South East', 1),
+ ('Lilongwe North West', 1),
+ ('Blantyre City South', 2),
+ ('Blantyre Kabula', 2),
+ ('Mzimba North', 3),
+ ('Mzimba South East', 3),
+ ('Zomba Central', 4),
+ ('Zomba Chisi', 4),
+ ('Mangochi Monkey Bay', 5),
+ ('Nsanje Lalanje', 6),
+ ('Kasungu East', 10),
+ ('Salima North West', 8);
+
+INSERT INTO Person ( RegNumber, FirstName, LastName, Gender,DateOfBirth) VALUES
+    ('BIS/23/SS/001', 'INNOCENT', 'CHAIMA', 'Male', '1990-03-07'),
+    ('BIS/23/SS/002', 'CLIFFORD', 'CHIBALE', 'Male', '1990-03-26'),
+    ('BIS/23/SS/003', 'BIZIWICK', 'CHIKUMBUTSO', 'Male', '1990-03-07'),
+    ('BIS/23/SS/004', 'IDAH', 'CHILONGO', 'Female', '1990-03-12'),
+    ('BIS/23/SS/005', 'MABLE', 'CHINGOTA', 'Female', '1990-03-05'),
+    ('BIS/22/SS/003', 'ANNA', 'CHIVUNDULA', 'Female', '1990-03-05'),
+    ('BIS/23/SS/007', 'ANNIE', 'CHIWONI', 'Female', '1990-02-25'),
+    ('BIS/23/SS/010', 'CHAULA', 'JULIUS', 'Male', '1990-02-20'),
+    ('BIS/22/SS/011', 'ANGELIQUE', 'KALANJE', 'Female', '1990-03-09'),
+    ('BIS/22/SS/014', 'CHISOMO', 'KANTSITSI', 'Female', '1990-03-04'),
+    ('BIS/23/SS/012', 'ENOCK', 'KAPHUKUSI', 'Male', '1990-03-06'),
+    ('BIS/23/SS/013', 'SANDRA', 'KASIRA', 'Female', '1990-03-12'),
+    ('BIS/22/SS/015', 'ALINAFE', 'KATANTHA', 'Female', '1990-03-12'),
+    ('BIS/23/SS/015', 'ANGELLA', 'KHOMBA', 'Female', '1990-03-06'),
+    ('BIS/23/SS/016', 'LUMBANI', 'LUHANGA', 'Male', '1990-03-05'),
+    ('BIS/23/SS/017', 'YANKHO', 'MAJAMANDA', 'Female', '1990-03-12'),
+    ('BIS/23/SS/018', 'Jonathan', 'MAKHULUDZO', 'Male', '1990-03-07'),
+    ('BIS/22/SS/017', 'PAULINE', 'MALONDA', 'Female', '1990-03-14'),
+    ('BIS/23/SS/020', 'DINGILE', 'MANDERE', 'Female', '1990-03-04'),
+    ('BIS/22/SS/021', 'ACKIM', 'MHONE', 'Male', '1990-03-10'),
+    ('BIS/23/SS/023', 'BLIX', 'MPUWE', 'Male', '1990-03-12'),
+    ('BIS/23/SS/024', 'SNOWDEN', 'MSUMBA', 'Male', '1990-03-05'),
+    ('BIS/22/ET/007', 'DOREEN', 'MTAMBO', 'Female', '1990-03-14'),
+    ('BIS/23/SS/028', 'HUGGINS', 'Mukwindidza', 'Male', '2000-02-25'),
+    ('BIS/22/SS/024', 'MIRIAM', 'MULERA', 'Female', '2000-03-18'),
+    ('BIS/23/SS/026', 'LAULYN', 'MWASE', 'Female', '2000-03-06'),
+    ('BIS/23/SS/027', 'ONGANI', 'MWASE', 'Male', '2000-03-12'),
+    ('BIS/23/SS/025', 'JOYCE', 'MWASE', 'Female', '2000-03-12'),
+    ('BIS/23/SS/030', 'SAMSON', 'PHANGULA', 'Male', '2000-03-12'),
+    ('BIS/22/SS/034', 'HELLEN', 'PHIRI', 'Female', '2000-03-06'),
+    ('BIS/23/SS/032', 'PATIENCE', 'PHIRI', 'Female', '2000-02-18'),
+    ('BIS/23/SS/031', 'CHANJU', 'PHIRI', 'Female', '2000-03-21'),
+    ('BIS/23/SS/033', 'ANNIE', 'RICHARD', 'Female', '2000-03-12'),
+    ('BIS/23/SS/034', 'JOHN', 'SAMBANI', 'Male', '2000-03-05'),
+    ('BIS/23/SS/036', 'AYANDA', 'SIMBEYE', 'Female', '2000-02-20'),
+    ('BIS/23/SS/037', 'CAROLINE', 'SINJA', 'Female', '2000-04-11'),
+    ('BIS/23/SS/038', 'LOYCE', 'SITOLO', 'Female', '2000-03-10'),
+    ('BIS/22/SS/036', 'TREVOR', 'SOKO', 'Male', '2000-03-10'),
+    ('BIS/23/SS/039', 'GEOFFREY', 'THINDWA', 'Male', '2000-03-29'),
+    ('BIS/23/SS/040', 'BRIAN', 'YONA', 'Male', '2000-02-21'),
+    ('BIT/23/SS/001', 'HOPE', 'ANDREW', 'Male', '2000-03-14'),
+    ('BIT/23/SS/002', 'Charles', 'BANDA', 'Male', '2000-03-05'),
+    ('BIT/22/SS/001', 'HANNAH', 'BOKO', 'Female', '2000-03-31'),
+    ('BIT/23/SS/003', 'EUNICE', 'CHIPETA', 'Female', '2000-02-26'),
+    ('BIT/21/SS/007', 'GLORY', 'CHIPUNGU', 'Female', '2002-03-14'),
+    ('BIT/23/SS/005', 'WILSON', 'CHIRWA', 'Male', '2002-03-06'),
+    ('BIT/23/SS/004', 'SHANIE', 'CHIRWA', 'Female', '2002-03-12'),
+    ('BIT/23/SS/006', 'MWIZA', 'GONDWE', 'Female', '2002-02-19'),
+    ('BIEP/23/SS/011', 'PATRICIA', 'HARRY', 'Female', '2002-03-14'),
+    ('BIT/23/SS/010', 'MIRRIAM', 'KALANDA', 'Female', '2002-03-05'),
+    ('BIT/23/SS/011', 'TIYANJANE', 'KAMDZEKA', 'Female', '2002-03-12'),
+    ('BIT/23/SS/012', 'CEDRIC', 'KAMENI', 'Male', '2002-02-20'),
+    ('BIT/23/SS/013', 'TENDAI', 'KAMWAMBA', 'Male', '2002-03-11'),
+    ('BIT/23/SS/014', 'LISSA', 'KAPONDA', 'Female', '2002-03-12'),
+    ('BIT/23/SS/015', 'LAWRENCE', 'KOLOKO', 'Male', '2002-03-05'),
+    ('BIT/23/SS/016', 'WONGANI', 'KONDOWE', 'Male', '2002-03-11'),
+    ('BIT/23/SS/017', 'LASTON', 'KUMWENDA', 'Male', '2002-03-14'),
+    ('BIT/23/SS/018', 'ELLEN', 'LIRADALA', 'Female', '2002-03-14'),
+    ('BIT/22/SS/018', 'CHISOMO', "M'BAWA", 'Female', '2002-03-12'),
+    ('BIT/23/SS/020', 'MERCY', 'MAKIYI', 'Female', '2002-03-28'),
+    ('BIT/23/SS/021', 'JOLEEN', 'MAKONDETSA', 'Female', '2002-03-06'),
+    ('BIT/23/SS/022', 'PRINCE', 'MANDALA', 'Male', '2002-03-05'),
+    ('BIT/23/SS/023', 'THOMAS', "MANONG'A", 'Male', '2003-02-28'),
+    ('BIT/23/SS/025', 'TUPOCHELE', 'MDEZA', 'Male', '2003-03-18'),
+    ('BIT/23/SS/026', 'VERA', 'MHANGO', 'Female', '2003-03-10'),
+    ('BIT/23/SS/044', 'ENOCK', 'MHURA', 'Male', '2003-03-05'),
+    ('BIT/23/SS/027', 'CHECK-US', 'MKANDAWIRE', 'Male', '2003-03-05'),
+    ('BIT/23/SS/028', 'MSAYIWALE', 'MOYA', 'Male', '2003-03-07'),
+    ('BIT/23/SS/030', 'LAURENCIA', 'MSEU', 'Female', '2003-04-01'),
+    ('BIT/23/SS/032', 'GIFT', 'MTHUZI', 'Male', '2003-03-11'),
+    ('BIT/22/SS/023', 'LUSAKO', 'MWAKALENGA', 'Male', '2003-03-12'),
+    ('BIT/23/SS/033', 'TIMOTHY', 'MWAMONDWE', 'Male', '2003-03-09'),
+    ('BIT/23/SS/036', 'WONGANI', 'PHIRI', 'Male', '2003-03-05'),
+    ('BIT/23/SS/037', 'UWICYEZA', 'SAIDAT', 'Female', '2003-03-03'),
+    ('BIT/23/SS/038', 'PEREZ', 'SAKA', 'Male', '2003-03-04'),
+    ('BIT/23/SS/040', 'TILIKA', 'TATYANA', 'Female', '2004-03-05'),
+    ('BIT/23/SS/041', 'MIKE', 'TILINGAMAWA', 'Male', '2004-03-12'),
+    ('BIT/23/SS/042', 'DALITSO', 'YAKOBE', 'Male', '2004-03-05'),
+    ('BIS/24/ME/144', 'TIONGE', 'BANDA', 'Female', '2004-03-22'),
+    ('BIS/24/ME/145', 'MAYAMIKO', 'CHIGWEDE', 'Male', '2004-02-26'),
+    ('BIS/24/ME/146', 'BERTHA', 'CHIKUMBU', 'Female', '2004-02-27'),
+    ('BIS/24/ME/147', 'GOMEZGANI', 'CHIRWA', 'Male', '2004-02-19'),
+    ('BIS/24/ME/148', 'EDDIE', 'GOCHO', 'Male', '2004-02-22'),
+    ('BIS/24/ME/150', 'BLESSINGS', 'MKANDAWIRE', 'Male', '2004-02-25'),
+    ('BIS/24/ME/151', 'MPHATSO', 'MLEME', 'Male', '2004-05-07'),
+    ('BIS/24/ME/152', 'SAMUEL', 'MPANDO', 'Male', '2004-02-28'),
+    ('BIS/24/ME/153', 'WANANGWA', 'MULENGA', 'Male', '2004-02-21'),
+    ('BIS/24/ME/154', 'KATIE', 'MUNTHALI', 'Female', '2004-02-18'),
+    ('BIS/24/ME/155', 'LUPAKISHO', 'MUSUKWA', 'Male', '2004-02-18'),
+    ('BIS/24/ME/156', 'THULANI', 'MZEMBE', 'Male', '2004-02-19'),
+    ('BIS/24/ME/159', 'NOEL', 'SAFARAWO', 'Male', '2004-03-21'),
+    ('BIS/24/ME/160', 'SAM', 'THEU', 'Male', '2004-02-28'),
+    ('BIT/24/ME/006', 'ISHAQ', 'ASSIMA', 'Male', '2004-02-23'),
+    ('BIT/23/ME/050', 'CHAWANANGWA', 'CHIKUNI', 'Male', '2005-02-21'),
+    ('BIT/24/ME/007', 'AUGUSTINE', 'KACHINGWE', 'Male', '2005-02-18'),
+    ('BIT/24/ME/010', 'VICTOR', 'MANDAWALA', 'Male', '2005-03-12'),
+    ('BIT/24/ME/011', 'RODNEY', 'MASEKO', 'Male', '2005-02-18'),
+    ('BIT/24/ME/014', 'THANDIWE', 'PHIRI', 'Female', '2005-02-19'),
+    ('BIT/24/ME/015', 'MICHEAL', 'SAMBAKUSI', 'Male', '2005-02-21'),
+    ('MSE/21/SS/001', 'CHISOMO', 'AFFONSO', 'Male', '2005-03-05'),
+    ('MSE/23/ME/002', 'ALEX', 'CHAKWANIRA', 'Male', '2005-02-18'),
+    ('MSE/21/SS/004', 'MELVIN', 'CHIKANAMOYO', 'Male', '2005-03-05'),
+    ('MSE/23/ME/004', 'Juwadu', 'Juma', 'Male', '2005-02-25'),
+    ('MSE/21/SS/016', 'WATIPA', 'KALULU', 'Female', '2005-03-06'),
+    ('MSE/21/SS/017', "ANGEL", "KAM'BWEMBA", 'Female', '2005-03-05'),
+    ('MSE/19/SS/014', 'MCNAIR', 'KAMANGA', 'Male', '2005-03-05'),
+    ('MSE/21/SS/020', 'ALINAFE', 'KUMILAMBE', 'Female', '2005-03-02'),
+    ('MSE/21/SS/022', 'SHARON', 'KWAITANA', 'Female', '2005-02-19'),
+    ('MSE/21/SS/023', 'JENIFER', 'MAGWAZA', 'Female', '2005-03-06'),
+    ('MSE/21/SS/026', 'HAJRA', 'MASUNGU', 'Female', '2005-03-06'),
+    ('MSE/21/SS/029', 'PYPHIAS', 'MHONE', 'Male', '2005-03-05'),
+    ('MSE/21/SS/035', 'FRANK', 'MWAKASUNGULA', 'Male', '2005-03-05'),
+    ('MSE/21/SS/040', 'VYSON', 'NSINI', 'Male', '2005-03-06'),
+    ('MSE/21/SS/045', 'INNOCENT', 'PHIRI', 'Male', '2005-03-06'),
+    ('MSE/21/SS/047', 'SINOYA', 'PHIRI', 'Male', '2005-03-06'),
+    ('MSE/21/SS/046', 'RICHARD', 'PHIRI', 'Male', '2005-03-06'),
+    ('MSE/20/SS/033', 'SAYNAT', 'SINJONJO', 'Female', '2005-03-05'),
+    ('MSE/20/SS/035', 'RABECCA', 'TEMBO', 'Female', '2005-03-12'),
+    ('BIT/24/ME/012', 'KENNEDY', 'MKANDAWIRE', 'Male', '2005-02-18'),
+    ('BIT/23/ME/069', 'GRACIOUS', 'NGUWO', 'Male', '2005-05-07');
+
+INSERT INTO Voter (PersonalID, VoterRegistrationNumber, Status, RegistrationDate, Category, ConstituencyID, WardID) VALUES
+(100, 'MSE/21/SS/001', 'Registered', '2025-09-29', 'SS', 2, 2),
+(101, 'MSE/23/ME/002', 'Not Registered', '2025-09-29', 'SS', 2, 2),
+(102, 'MSE/21/SS/004', 'Registered', '2025-09-29', 'SS', 2, 2),
+(103, 'MSE/23/ME/004', 'Registered', '2025-09-29', 'SS', 2, 2);
+
+INSERT INTO Voter (PersonalID, VoterRegistrationNumber, Status, RegistrationDate, Category, ConstituencyID, WardID) VALUES
+(104, 'MSE/21/SS/016', 'Registered', '2025-09-29', 'SS', 1, 1),
+(105, 'MSE/21/SS/017', 'Registered', '2025-09-29', 'SS', 1, 1),
+(106, 'MSE/19/SS/014', 'Registered', '2025-09-29', 'SS', 1, 1),
+(107, 'MSE/21/SS/020', 'Not Registered', '2025-09-29', 'SS', 1, 1);
+
+INSERT INTO Voter (PersonalID, VoterRegistrationNumber, Status, RegistrationDate, Category, ConstituencyID, WardID) VALUES
+(108, 'MSE/21/SS/022', 'Registered', '2025-09-29', 'SS', 1, 1),
+(109, 'MSE/21/SS/023', 'Registered', '2025-09-29', 'SS', 1, 1),
+(110, 'MSE/21/SS/026', 'Registered', '2025-09-29', 'SS', 1, 1);
+
+INSERT INTO Voter (PersonalID, VoterRegistrationNumber, Status, RegistrationDate, Category, ConstituencyID, WardID) VALUES
+(111, 'MSE/21/SS/029', 'Registered', '2025-09-29', 'SS', 2, 2),
+(112, 'MSE/21/SS/035', 'Not Registered', '2025-09-29', 'SS', 2, 2),
+(113, 'MSE/21/SS/040', 'Registered', '2025-09-29', 'SS', 2, 2);
+
+INSERT INTO Voter (PersonalID, VoterRegistrationNumber, Status, RegistrationDate, Category, ConstituencyID, WardID) VALUES
+(114, 'MSE/21/SS/045', 'Registered', '2025-09-29', 'SS', 1, 1),
+(115, 'MSE/21/SS/047', 'Registered', '2025-09-29', 'SS', 1, 1),
+(116, 'MSE/21/SS/046', 'Registered', '2025-09-29', 'SS', 1, 1);
+
+INSERT INTO Voter (PersonalID, VoterRegistrationNumber, Status, RegistrationDate, Category, ConstituencyID, WardID) VALUES
+(117, 'MSE/20/SS/033', 'Not Registered', '2025-09-29', 'SS', 2, 2),
+(118, 'MSE/20/SS/035', 'Registered', '2025-09-29', 'SS', 2, 2),
+(119, 'BIT/24/ME/012', 'Registered', '2025-09-29', 'SS', 2, 2);
+
+
+
+
+INSERT INTO Candidate (PersonalID, PartyID, Position)
+VALUES
+  (99, 1, 'President'),
+  (100, 2, 'President'),
+  (101, 3, 'President'),
+  (102, 4, 'President'),
+  (103, 5, 'President'),
+  (104, 6, 'President'),
+  (105, 7, 'President'),
+  (106, 8, 'President'),
+  (107, 9, 'President');
+
+
+INSERT INTO Candidate (PersonalID, PartyID, Position, ConstituencyID)
+VALUES
+  (108, 2, 'MP', 1),
+  (109, 3, 'MP', 1),
+  (110, 1, 'MP', 1);
+
+INSERT INTO Candidate (PersonalID, PartyID, Position, ConstituencyID)
+VALUES
+  (111, 2, 'MP', 2),
+  (112, 3, 'MP', 2),
+  (113, 1, 'MP', 2);
+
+
+  INSERT INTO Candidate (PersonalID, PartyID, Position, ConstituencyID, WardID)
+VALUES
+  (114, 1, 'Councillor', 1, 1),
+  (115, 2, 'Councillor', 1, 1),
+  (116, 3, 'Councillor', 1, 3);
+
+   INSERT INTO Candidate (PersonalID, PartyID, Position, ConstituencyID, WardID)
+VALUES
+  (117, 4, 'Councillor', 2, 2),
+  (118, 5, 'Councillor', 2, 2),
+  (119, 6, 'Councillor', 2, 2);
